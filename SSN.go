e18b1a3c32c0a12ef3c003b1062b8b78d1c913af
Alex Jose Bujosa Cruz <bujosa2014@outlook.com>
2022-05-17 03:22:03 -0400

    tercer commit

--- SSN.go
+++ SSN.go
@@ -13,6 +13,16 @@ var pos = 0
 func Splitstring(text string, pos int) []string{
 	return strings.Split(text, symbol[pos])
 }
+func removeEmpty(arr []string) []string{
+	var newArr []string
+	for i:=0; i< len(arr); i++{
+		if arr[i] != "" {
+			newArr = append(newArr, arr[i])
+		}
+	}
+	return newArr
+
+}
 func validate(validatorSplit []string, input string, pos int, iteration int) string{
 	if len(validatorSplit) == 0 {
 		validatorSplit = Splitstring(validator, pos)
@@ -25,7 +35,37 @@ func validate(validatorSplit []string, input string, pos int, iteration int) str
 		if len(input) != num  || err != nil{
 			return "Social Security Number not valid"
 		}
-		
+		rtrn := validate(validateSpl, input, pos, iteration)
+		return rtrn
+	} else if pos == 1{
+		pos += 1
+		validateSpl := Splitstring(validatorSplit[0], pos)
+		validateSpl = removeEmpty(validateSpl)
+		for i:= 0; i<len(validateSpl); i++ {
+			_, err := strconv.Atoi(validateSpl[i])
+			if err != nil {
+				valSplit := Splitstring(validateSpl[i], pos +1)
+				if len(validateSpl) == 1 {
+					rtrn := validate(valSplit, input, pos + 1, iteration)
+					return rtrn
+				}else {
+					rtrn :=  validate(valSplit, input, pos, iteration)
+					return rtrn
+				}
+			}
+			if validateSpl[i] == input {
+				return "Social Security Number not valid"
+			}
+		}
+	} else {
+		firstNumber, _ := strconv.Atoi(validatorSplit[0])
+		secondNumber, _ := strconv.Atoi(validatorSplit[1])
+		ssn, _ := strconv.Atoi(input)
+		if (firstNumber <= ssn && secondNumber >= ssn) && pos == 2 {
+			return "Social Security Number not valid"
+		} else if (firstNumber > ssn || secondNumber < ssn) && pos != 2{
+			return "Social Security Number not valid"
+		}
 	}
 	return "Social Security Number valid"
 }
@@ -34,7 +74,7 @@ func main(){
 	fmt.Print("Ingrese su numero social: ")
 	reader.Scan()
 	input := reader.Text()
-	splitinput := Splitstring(input, pos)
+	splitinput := strings.Split(input, "-")
 	var validatorSplit []string
 	var message string
 	for i:= 0; i< len(splitinput); i++ {
